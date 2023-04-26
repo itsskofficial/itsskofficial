5651a5237baab497e9a5b88d2ed591846fd01343
SK <sarthakkarandikar03@gmail.com>
2023-04-26 00:20:05 +0000

    Apr 26, 2023, 5:50 AM

---
+++ components/NavBar.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const NavBar = () => {
+    return (
+        <ul className={navlist}>
+            <Link
+        </ul>
+         
+    )
+}
+
+export default NavBar
