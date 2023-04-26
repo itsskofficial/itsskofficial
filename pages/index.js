c9527b6c38dcaed7e0645425f07bbdbfe27d2053
SK <sarthakkarandikar03@gmail.com>
2023-04-26 00:16:31 +0000

    Apr 26, 2023, 5:46 AM

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <
+        <NavBar/>
       </main>
     </>
   )
