b088f3de4e72a9e81939256c0be72b616739e76b
SK <sarthakkarandikar03@gmail.com>
2023-04-26 00:49:13 +0000

    Apr 26, 2023, 6:19 AM

--- components/NavBar.js
+++ components/NavBar.js
@@ -17,6 +17,7 @@ const NavBar = () => {
                 <li>
                     {navlinks.map(link => {
                         <li>
+                            link
                     })}
                     Hello
                 </li>
