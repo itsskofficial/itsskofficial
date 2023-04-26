2471748dc71497e569a055e25c3d76efa7fa2b78
SK <sarthakkarandikar03@gmail.com>
2023-04-26 00:26:29 +0000

    Apr 26, 2023, 5:56 AM

--- components/NavBar.js
+++ components/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () => {
         <Fragment>
             <ul className={navlist}>
                 {navlinks.map(link => {
-                    <Link className={navlink}
+                    <Link href=`className={navlink}
                 })}
             </ul>
         </Fragment>
