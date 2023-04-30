ec874c45db9c87859d6835a12c70c58985c87828
SK <sarthakkarandikar03@gmail.com>
2023-04-30 12:25:39 +0000

    Apr 30, 2023, 5:55 PM

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className={headtext}>
             <h1>
-                I am Sarthak
+                Hey, I am Sarthak
             </h1>
         </div>
     )
