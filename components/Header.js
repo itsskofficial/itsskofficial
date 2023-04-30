7484edb89021d2d9a2e2090fc2b8b7b981a0c69f
SK <sarthakkarandikar03@gmail.com>
2023-04-30 12:38:42 +0000

    Apr 30, 2023, 6:08 PM

--- components/Header.js
+++ components/Header.js
@@ -4,10 +4,10 @@ const Header = () => {
     const roles = ["AI", "IOT", "Metaverse"]
     return (
         <div className={headtext}>
-            <h1 className={headtitle}>
+            <h1 className={headTitle}>
                 Hey, I'm Sarthak
             </h1>
-            <h2 className={headsubtitle}>
+            <h2 className={headSubtitle}>
                 I am an {roles[0]} developer
             </h2>
             <button className={headContact}
