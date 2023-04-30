505ac19f51c77e2a004d805126542a642ba73f92
SK <sarthakkarandikar03@gmail.com>
2023-04-30 14:05:02 +0000

    Apr 30, 2023, 7:35 PM

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import NavBar from '@components/NavBar'
+import Header from '@components/Header'
 
 export default function Home() {
   return (
@@ -11,7 +12,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <NavBar/>
+        <NavBar />
+        <Header />
       </main>
     </>
   )
