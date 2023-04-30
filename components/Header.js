cde02f5eca6ce27528734e62730ccd50ee539dda
SK <sarthakkarandikar03@gmail.com>
2023-04-30 12:31:34 +0000

    Apr 30, 2023, 6:01 PM

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,14 @@
 import styles from '@styles/Header.module.css'
 
 const Header = () => {
-    const roles = ["Web2", "Web3", "AI", "IOT", ""]
+    const roles = ["Web2", "Web3", "AI", "IOT"]
     return (
         <div className={headtext}>
             <h1>
                 Hey, I'm Sarthak
             </h1>
             <h2>
-                I am a
+                I am a 
             </h2>
         </div>
     )
