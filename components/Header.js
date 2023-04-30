d705285557dac591225c543d3bb6bbceb2acc29b
SK <sarthakkarandikar03@gmail.com>
2023-04-30 12:38:35 +0000

    Apr 30, 2023, 6:08 PM

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
             <h2 className={headsubtitle}>
                 I am an {roles[0]} developer
             </h2>
-            <button className={head}
+            <button className={headContact}
         </div>
     )
 }
