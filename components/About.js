5ff35cbdca0ab0e016820216aae434d37790dedb
SK <sarthakkarandikar03@gmail.com>
2023-05-01 02:10:37 +0000

    May 1, 2023, 7:40 AM

--- components/About.js
+++ components/About.js
@@ -9,7 +9,7 @@ const About = () => {
                     About
                 </h1>
                 <h2 className={styles.aboutInfo}>
-                    A passionate guy who likes all things digital, from design to coding to deployment. Enthusiast
+                    A passionate guy who likes all things digital, from design to coding to deployment. Majorly 
                 </h2>
             </div>
         </section>
