70827810c82bc42b62a9447cc95369e9f6e3199f
SK <sarthakkarandikar03@gmail.com>
2023-05-01 04:01:37 +0000

    May 1, 2023, 9:31 AM

--- components/ui/SkillSkin.js
+++ components/ui/SkillSkin.js
@@ -2,7 +2,7 @@ const { default: Image } = require("next/image")
 
 const SkillsSkin = (props) => {
     const code = ['html', 'css', 'js']
-    const tools = ['figma',]
+    const tools = ['figma', 'canva', 'gimp']
     return (
         <div className={styles.parent}>
             {props.option=='code'?renderIcons(code):renderIcons(tools)}
