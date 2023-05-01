18a43f002dd16eb37c18efcdfef11e763e0c7160
SK <sarthakkarandikar03@gmail.com>
2023-05-01 02:07:41 +0000

    May 1, 2023, 7:37 AM

--- components/About.js
+++ components/About.js
@@ -2,7 +2,7 @@ const About = () => {
     return (
         <section className={styles.parent}>
             <div className={styles.aboutImage}>
-                <Image src='/../public/assets/images'
+                <Image src='/../public/assets/images/aboutImage.jpg' width=''
             </div>
         </section>
     )
