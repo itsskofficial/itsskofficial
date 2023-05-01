067d3be076065892f45370df2bf5f8b925bff443
SK <sarthakkarandikar03@gmail.com>
2023-05-01 09:23:04 +0000

    May 1, 2023, 2:53 PM

--- components/Contact.js
+++ components/Contact.js
@@ -1,8 +1,11 @@
+const { default: Link } = require("next/link")
+
 const Contact = () => {
     return (
         <section className={parent}>
             <div className={contactIcons}>
-                <i class='fa-brands fa-linkedin fa-4x' href/>
+            <Link href=''
+                <i class='fa-brands fa-linkedin fa-4x'/>
 
             </div>
         </section>
