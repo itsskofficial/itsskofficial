bb229b5be8180e1738ccce561a9789eabd31bbdd
SK <sarthakkarandikar03@gmail.com>
2023-05-01 02:08:32 +0000

    May 1, 2023, 7:38 AM

--- components/About.js
+++ components/About.js
@@ -4,6 +4,9 @@ const About = () => {
             <div className={styles.aboutImage}>
                 <Image src='/../public/assets/images/aboutImage.jpg' width='400' height='500' alt='About Image' />
             </div>
+            <div className={styles.aboutText}>
+                
+            </div>
         </section>
     )
 }
