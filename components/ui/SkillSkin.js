f48a6405bc79b63874873b57cd1d5440cb63b0d7
SK <sarthakkarandikar03@gmail.com>
2023-05-01 04:02:23 +0000

    May 1, 2023, 9:32 AM

--- components/ui/SkillSkin.js
+++ components/ui/SkillSkin.js
@@ -1,6 +1,8 @@
 const SkillsSkin = (props) => {
     const code = ['html', 'css', 'js']
     const tools = ['figma', 'canva', 'gimp']
+
+    const 
     return (
         <div className={styles.parent}>
             {props.option=='code'?renderIcons(code):renderIcons(tools)}
