49db1ccbc8e92a669fe6ed943193d739ccb7929a
SK <sarthakkarandikar03@gmail.com>
2023-05-01 09:04:20 +0000

    May 1, 2023, 2:34 PM

--- components/ui/SkillsSkin.js
+++ components/ui/SkillsSkin.js
@@ -7,7 +7,7 @@ const SkillsSkin = (props) => {
                 <i class='fa-brands fa-python fa-3x' />
                 <i class='fa-brands fa-js fa-3x' />
                 <i class='fa-brands fa-c fa-3x' />
-                <Image src='/../public/assets/images/cpp.svg' width='50' heigh='50' alt='cpp'>
+                <Image src='/../public/assets/images/cpp.svg' width='50' heigh='50' alt='CPP Icon'>
                 <i class='fa-brands fa-html5 fa-3x' />
                 <i class='fa-brands fa-css3 fa-3x' />
             </div>
