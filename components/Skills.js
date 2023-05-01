417e9e931fcf35e877d942215ae62cfdb9a74a58
SK <sarthakkarandikar03@gmail.com>
2023-05-01 07:31:50 +0000

    May 1, 2023, 1:01 PM

--- components/Skills.js
+++ components/Skills.js
@@ -10,15 +10,8 @@ const Skills = () => {
                 <h1 className={styles.skillsTitle}>
                     Skills
                 </h1>
-                <div class="button-cover">
-                    <div class="button b2" id="button-10">
-                        <input type="checkbox" class="checkbox" />
-                        <div class="knobs">
-                            <span>Code</span>
-                        </div>
-                        <div class="layer"></div>
-                    </div>
-                </div>
+                <div >
+
             </div>
             <div className={styles.skillsIcons}>
                 <SkillsSkin option={skillsOption} />
