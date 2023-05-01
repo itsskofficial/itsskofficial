0434db1338e9f82f143699eafd5c24c0ddaa27f2
SK <sarthakkarandikar03@gmail.com>
2023-05-01 09:23:11 +0000

    May 1, 2023, 2:53 PM

--- components/Contact.js
+++ components/Contact.js
@@ -4,7 +4,7 @@ const Contact = () => {
     return (
         <section className={parent}>
             <div className={contactIcons}>
-            <Link href=''
+            <Link href='https://linkedin.com/'
                 <i class='fa-brands fa-linkedin fa-4x'/>
 
             </div>
