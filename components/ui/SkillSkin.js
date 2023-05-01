cebfe6407900a37f80406bcbe4e42ec095895391
SK <sarthakkarandikar03@gmail.com>
2023-05-01 04:00:34 +0000

    May 1, 2023, 9:30 AM

--- components/ui/SkillSkin.js
+++ components/ui/SkillSkin.js
@@ -4,7 +4,7 @@ const SkillsSkin = (props) => {
     const code = ['html', 'css', 'js']
     return (
         <div className={styles.parent}>
-            {props.}
+            {props.option=='code'}
         </div>
     )
 }
