6f735aba369b800630716425891f84275cb4b2f3
SK <sarthakkarandikar03@gmail.com>
2023-05-01 05:14:46 +0000

    May 1, 2023, 10:44 AM

--- components/Skills.js
+++ components/Skills.js
@@ -11,24 +11,9 @@ const Skills = () => {
                     Skills
                 </h1>
                 <div className={skillsOptionCover}>
-                    
+
                 </div>
-                <button className={styles.skillsOption} onClick={() => {
-                    if (skillsOption == 'code') {
-                        setSkillsOption('tools')
-                    }
-                    else{
-                        setSkillsOption('code')
-                    }
-                }}>
-                    <input type="checkbox" className={styles.skillsOptionCheckbox} />
-                    <div className={styles.skillsOptionKnob} >
-                        <span>
-                            Code
-                        </span>
-                    </div>
-                    <div className={styles.skillsOptionLayer} />
-                </button>
+                
             </div>
             <div className={styles.skillsIcons}>
                 <SkillsSkin option={skillsOption} />
