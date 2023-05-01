7cb220579298704eb903e3d011211858cfa88650
SK <sarthakkarandikar03@gmail.com>
2023-05-01 09:22:48 +0000

    May 1, 2023, 2:52 PM

--- components/Contact.js
+++ components/Contact.js
@@ -2,7 +2,7 @@ const Contact = () => {
     return (
         <section className={parent}>
             <div className={contactIcons}>
-                <i class='fa-brands fa-linkedin fa-5x'
+                <i class='fa-brands fa-linkedin fa-4x' href/>
 
             </div>
         </section>
