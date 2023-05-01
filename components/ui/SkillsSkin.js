14b95eaf66e96201e8b17ae4743145e351cd4936
SK <sarthakkarandikar03@gmail.com>
2023-05-01 07:46:21 +0000

    May 1, 2023, 1:16 PM

--- components/ui/SkillsSkin.js
+++ components/ui/SkillsSkin.js
@@ -7,7 +7,8 @@ const SkillsSkin = (props) => {
     const codeIcons = () => {
         return (
             <div className={skillsSkinRow}>
-                <i class='fa-brands fa-react fa-3x'/>
+                <i class='fa-brands fa-react fa-3x' />
+                
             </div>
         )
     }
