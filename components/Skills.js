f2bd075895d3ca60185c688c546b8f5b37f72d00
SK <sarthakkarandikar03@gmail.com>
2023-05-01 07:32:40 +0000

    May 1, 2023, 1:02 PM

--- components/Skills.js
+++ components/Skills.js
@@ -11,7 +11,7 @@ const Skills = () => {
                     Skills
                 </h1>
                 <div className={styles.skillsOptions}>
-                    </div>
+                </div>
 
             </div>
             <div className={styles.skillsIcons}>
