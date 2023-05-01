005fce28f63693032b774bf743d703a4d2892357
SK <sarthakkarandikar03@gmail.com>
2023-05-01 09:21:05 +0000

    May 1, 2023, 2:51 PM

--- components/ui/SkillsSkin.js
+++ components/ui/SkillsSkin.js
@@ -17,15 +17,15 @@ const SkillsSkin = (props) => {
                 <i class='fa-brands fa-css3 fa-3x' />
                 <i class='fa-brands fa-react fa-3x' />
                 <i class='fa-brands fa-node fa-3x' />
-                <Image src='/../public/assets/images/esjs.svg' width='50' heigh='50' alt='EJs Icon'/>
-                <Image src='/../public/assets/images/mongodb.svg' width='50' heigh='50' alt='EJs Icon'/>
+                <Image src='/../public/assets/images/esjs.svg' width='50' height='50' alt='EJs Icon'/>
+                <Image src='/../public/assets/images/mongodb.svg' width='50' height='50' alt='EJs Icon'/>
             </div>
             <div className = {styles.skillsSkinRow } >
-                <Image src='/../public/assets/images/tf.svg' width='50' heigh='50' alt='EJs Icon'/>
+                <Image src='/../public/assets/images/tf.svg' width='50' height='50' alt='EJs Icon'/>
                 <i class='fa-brands fa-react fa-3x' />
                 <i class='fa-brands fa-node fa-3x' />
-                <Image src='/../public/assets/images/esjs.svg' width='50' heigh='50' alt='EJs Icon'/>
-                <Image src='/../public/assets/images/mongodb.svg' width='50' heigh='50' alt='EJs Icon'/>
+                <Image src='/../public/assets/images/esjs.svg' width='50' height='50' alt='EJs Icon'/>
+                <Image src='/../public/assets/images/mongodb.svg' width='50' height='50' alt='EJs Icon'/>
             </div>
         </Fragment>
             
