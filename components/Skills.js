a19c5c3d0b04ea84852eb89e4d7e2c36d479fb63
SK <sarthakkarandikar03@gmail.com>
2023-05-01 03:31:50 +0000

    May 1, 2023, 9:01 AM

--- components/Skills.js
+++ components/Skills.js
@@ -2,7 +2,7 @@ const Skills = () => {
     return (
         <section className={styles.parent}>
             <div className={styles.skillsText}>
-                <h1>
+                <h1 className={styles.skillsTitle}>
                     Skills
                 </h1>
             </div>
