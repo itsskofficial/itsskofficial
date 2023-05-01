4fe4a823474d54007fdd49707b8631f71ad5c0c5
SK <sarthakkarandikar03@gmail.com>
2023-05-01 09:19:19 +0000

    May 1, 2023, 2:49 PM

--- components/ui/SkillsSkin.js
+++ components/ui/SkillsSkin.js
@@ -21,7 +21,7 @@ const SkillsSkin = (props) => {
                 <Image src='/../public/assets/images/mongodb.svg' width='50' heigh='50' alt='EJs Icon'/>
             </div>
             <div className = {styles.skillsSkinRow } >
-                <Image src='/../public/assets/images/esjs.svg' width='50' heigh='50' alt='EJs Icon'/>
+                <Image src='/../public/assets/images/tf.svg' width='50' heigh='50' alt='EJs Icon'/>
                 <i class='fa-brands fa-react fa-3x' />
                 <i class='fa-brands fa-node fa-3x' />
                 <Image src='/../public/assets/images/esjs.svg' width='50' heigh='50' alt='EJs Icon'/>
