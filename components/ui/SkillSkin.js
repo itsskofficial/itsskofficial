16e958df5ea4bda61da32d3baa1aacae76889528
SK <sarthakkarandikar03@gmail.com>
2023-05-01 03:22:06 +0000

    May 1, 2023, 8:52 AM

--- components/ui/SkillSkin.js
+++ components/ui/SkillSkin.js
@@ -1,7 +1,9 @@
+const { default: Image } = require("next/image")
+
 const SkillSkin = () => {
     return (
         <div className={styles.skillBox}>
-            
+            <Image
         </div>
     )
 }
