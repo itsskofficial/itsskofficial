d64878af1975ab7be798c349ab01ee9f8835b715
SK <sarthakkarandikar03@gmail.com>
2023-05-01 02:03:50 +0000

    May 1, 2023, 7:33 AM

--- components/Header.js
+++ components/Header.js
@@ -20,7 +20,7 @@ const Header = () => {
                 </button>
             </div>
             <div className={styles.headImage}>
-                <Image src='/../public/assets/image/headImage.jpg' width='300' height='300' alt='Hero Image'/>
+                <Image src='/public/assets/image/headImage.jpg' width='300' height='300' alt='Hero Image'/>
             </div>
         </div>
         
