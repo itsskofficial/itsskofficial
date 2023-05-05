0f168b96e173605a0e6952395f0fec8b47af5778
SK <sarthakkarandikar03@gmail.com>
2023-05-05 05:44:16 +0000

    May 5, 2023, 11:14 AM

--- components/Skills.js
+++ components/Skills.js
@@ -14,7 +14,7 @@ const Skills = () => {
                     Skills
                 </h1>
                 <div className={styles.skillsToggle}>
-                    <button className={[styles.skillsCode} onClick={() => {
+                    <button className={[styles.skillsCode,} onClick={() => {
                         if (skillsOption != 'code') {
                             setSkillsOption('code')
                         }
