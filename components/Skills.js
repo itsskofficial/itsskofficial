580c7c5d447933455eabfb4e1afed95678133ac9
SK <sarthakkarandikar03@gmail.com>
2023-05-05 08:41:53 +0000

    May 5, 2023, 2:11 PM

--- components/Skills.js
+++ components/Skills.js
@@ -32,7 +32,7 @@ const Skills = () => {
                 </div>
             </div>
             {optionChanged ?
-                <div className={styles.skillsIcons}>
+                <div className={[styles.skillsIcons, ]}>
                     <SkillsSkin option={skillsOption} />
                 </div> :
                 }
