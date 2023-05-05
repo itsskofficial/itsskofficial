3649450eab2535304fa795e83bb7b5ccb8ac4f2b
SK <sarthakkarandikar03@gmail.com>
2023-05-05 08:17:28 +0000

    May 5, 2023, 1:47 PM

--- components/Skills.js
+++ components/Skills.js
@@ -12,7 +12,24 @@ const Skills = () => {
                 </h1>
                 {useEffect(()=> {
                     return (
-                        
+                        <div className={styles.skillsToggle}>
+                        <button className={[styles.skillsCode,skillsOption=='code'?styles.skillsActiveButton:null].join(' ')} onClick={() => {
+                            if (skillsOption != 'code') {
+                                setSkillsOption('code')
+                            }
+                            
+                                
+                        }}>
+                            <i class='fa-solid fa-code' style={{marginRight: '10px'}} />Code
+                        </button>
+                        <button className={[styles.skillsTools,skillsOption=='tools'?styles.skillsActiveButton:null].join(' ')} onClick={() => {
+                            if (skillsOption != 'tools') {
+                                setSkillsOption('tools')
+                            }
+                        }}>
+                            <i class='fa-solid fa-screwdriver-wrench' style={{marginRight: '10px'}} />Tools
+                        </button>
+                    </div>
                     )
                 })}
                 <div className={styles.skillsToggle}>
