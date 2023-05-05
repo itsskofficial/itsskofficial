2bc381b33c5731ec423acb17987c4c299a825e1e
SK <sarthakkarandikar03@gmail.com>
2023-05-05 05:51:33 +0000

    May 5, 2023, 11:21 AM

--- components/Skills.js
+++ components/Skills.js
@@ -4,10 +4,10 @@ import { useState } from 'react'
 
 const Skills = () => {
     const [skillsOption, setSkillsOption] = useState('code')
-    var codeButtonClasses = classNames([
-        'styles.skillsCode': true,
+    // var codeButtonClasses = classNames([
+    //     'styles.skillsCode': true,
         
-    ])
+    // ])
     return (
         <section className={styles.parent}>
             <div className={styles.skillsText}>
