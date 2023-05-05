49557eab0775533a022d2f80304e6fc0d78c9bc0
SK <sarthakkarandikar03@gmail.com>
2023-05-05 04:58:42 +0000

    May 5, 2023, 10:28 AM

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,7 @@
 const Footer = () => {
     return (
         <span>
-            <link
-  rel="stylesheet"
-  href="https://unicons.iconscout.com/release/v4.0.0/css/unicons.css"
-/>
+            8364
         </span>
     )
 }
