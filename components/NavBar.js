f29b54dce58d8132716da3182baab7d73c495df9
SK <sarthakkarandikar03@gmail.com>
2023-05-05 09:33:46 +0000

    May 5, 2023, 3:03 PM

--- components/NavBar.js
+++ components/NavBar.js
@@ -31,7 +31,7 @@ const NavBar = () => {
                     return(
                         <a href={`/#${link.toLowerCase()}`} className={styles.navLink} onClick={() => {
                             navlinks.forEach((link) => {
-                               link['classes'] = {styles.navLink}
+                               link['classes'] = [styles.navLink]
                             })
                         }}>
                             {link['name']}
