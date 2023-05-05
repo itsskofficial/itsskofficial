7e28cc8e763d5e7ea847aa51b1e634604cc1e429
SK <sarthakkarandikar03@gmail.com>
2023-05-05 04:35:52 +0000

    May 5, 2023, 10:05 AM

--- components/Contact.js
+++ components/Contact.js
@@ -13,7 +13,7 @@ const Contact = () => {
                 </Link>
             </div>
             <div className=[styles.contactText]>
-                <h1
+                <h2
         </section>
     )
 }
