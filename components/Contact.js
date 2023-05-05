23ad675045dd8be235ef162e07d1573376683d8a
SK <sarthakkarandikar03@gmail.com>
2023-05-05 04:44:39 +0000

    May 5, 2023, 10:14 AM

--- components/Contact.js
+++ components/Contact.js
@@ -11,6 +11,9 @@ const Contact = () => {
                 <Link href='mailto:sarthakkarandikar03@gmail.com'>
                     <i class='fa-solid fa-envelope fa-4x'/>
                 </Link>
+                <Link href='mailto:sarthakkarandikar03@gmail.com'>
+                    <i class='fa-solid fa-envelope fa-4x'/>
+                </Link>
             </div>
             <div className={styles.contactText}>
                 <h2 className={styles.contactTitle}>
