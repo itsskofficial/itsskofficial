93504bf6b094ba8ce49c31009ff2511e4b6a2212
SK <sarthakkarandikar03@gmail.com>
2023-05-05 08:20:02 +0000

    May 5, 2023, 1:50 PM

--- components/Skills.js
+++ components/Skills.js
@@ -1,6 +1,6 @@
 import styles from '@styles/Skills.module.css'
 import SkillsSkin from './ui/SkillsSkin'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 const Skills = () => {
     const [skillsOption, setSkillsOption] = useState('code')
@@ -10,26 +10,7 @@ const Skills = () => {
                 <h1 className={styles.skillsTitle}>
                     Skills
                 </h1>
-                {useEffect(()=> {
-                    return (
-                        <div className={styles.skillsToggle}>
-                            <button className={[styles.skillsCode,skillsOption=='code'?styles.skillsActiveButton:null].join(' ')} onClick={() => {
-                                if (skillsOption != 'code') {
-                                    setSkillsOption('code')
-                                }      
-                            }}>
-                                <i class='fa-solid fa-code' style={{marginRight: '10px'}} />Code
-                            </button>
-                            <button className={[styles.skillsTools,skillsOption=='tools'?styles.skillsActiveButton:null].join(' ')} onClick={() => {
-                                if (skillsOption != 'tools') {
-                                    setSkillsOption('tools')
-                                }
-                            }}>
-                                <i class='fa-solid fa-screwdriver-wrench' style={{marginRight: '10px'}} />Tools
-                            </button>
-                        </div>
-                    )
-                }),[skill]}
+                
                 <div className={styles.skillsToggle}>
                     <button className={[styles.skillsCode,skillsOption=='code'?styles.skillsActiveButton:null].join(' ')} onClick={() => {
                         if (skillsOption != 'code') {
