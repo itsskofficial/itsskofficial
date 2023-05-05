e65c064279a795b602502af47752e06303377c7f
SK <sarthakkarandikar03@gmail.com>
2023-05-05 04:27:31 +0000

    May 5, 2023, 9:57 AM

--- components/Contact.js
+++ components/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
                 <Link href='https://linkedin.com/'>
                     <i class='fa-brands fa-linkedin fa-4x'/>
                 </Link>
-                <Link href='mailto:'>
+                <Link href='mailto:sk@itsskofficial.com'>
                     <i class='fa-brands fa-envelope fa-4x'/>
                 </Link>
             </div>
