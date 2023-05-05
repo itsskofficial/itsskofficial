a35674e67197a74768149450ed842d5e47ca0898
SK <sarthakkarandikar03@gmail.com>
2023-05-05 09:45:48 +0000

    May 5, 2023, 3:15 PM

--- components/NavBar.js
+++ components/NavBar.js
@@ -33,7 +33,7 @@ const NavBar = () => {
                             navlinks.forEach((link) => {
                                link['classes'] = [styles.navLink]
                             })
-                            navLin
+                            navlinks[0]['classes']=[sty]
                             link['classes'] = [styles.navLink, styles.navLinkActive].join(' ')
                             console.log(navlinks[0]['classes'])
                         }}>
