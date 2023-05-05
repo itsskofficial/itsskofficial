8785dbdd6cf17cf98947ab1612f3856e312d36e6
SK <sarthakkarandikar03@gmail.com>
2023-05-05 05:01:11 +0000

    May 5, 2023, 10:31 AM

--- components/Footer.js
+++ components/Footer.js
@@ -2,11 +2,10 @@ const Footer = () => {
     return (
         <section className={styles.parent}>
             <div className={styles.footerText}>
-                <span className={}>
+                <span className={styles.footer}>
                     &#8364
                 </span>
             </div>
-            
         </section>
     )
 }
