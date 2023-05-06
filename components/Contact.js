a103ca926f3785477ef951f70d40f75682125b5f
SK <sarthakkarandikar03@gmail.com>
2023-05-06 13:14:54 +0000

    May 6, 2023, 6:44 PM

--- components/Contact.js
+++ components/Contact.js
@@ -26,6 +26,29 @@ const Contact = forwardRef((props,ref) => {
                 </h3>
             </div>
         </section>
+
+    const smallScreen =
+        <section ref={ref} id='contact' className={styles.parent}>
+            <div className={styles.contactIcons}>
+                <Link href='https://linkedin.com/'>
+                    <i class='fa-brands fa-linkedin fa-3x'/>
+                </Link>
+                <Link href='mailto:sarthakkarandikar03@gmail.com'>
+                    <i class='fa-solid fa-envelope fa-3x'/>
+                </Link>
+                <Link href='tel:+918275017823'>
+                    <i class='fa-solid fa-phone fa-3x'/>
+                </Link>
+            </div>
+            <div className={styles.contactText}>
+                <h2 className={styles.contactTitle}>
+                    Contact
+                </h2>
+                <h3 className={styles.contactInfo}>
+                    I am available on LinkedIn for discussions and stuff. If LinkedIn is not convenient you can mail me too
+                </h3>
+            </div>
+        </section>
     return (
         
     )
