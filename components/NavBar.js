4cdf1b6af0fad01b656c2178c9f475259264282e
SK <sarthakkarandikar03@gmail.com>
2023-05-06 12:33:14 +0000

    May 6, 2023, 6:03 PM

--- components/NavBar.js
+++ components/NavBar.js
@@ -97,7 +97,7 @@ const NavBar = (props) => {
             <h2 className={styles.navbarLogo}>
                 SK
             </h2>
-            <Menu right width={150}>
+            <Menu className= {right width={150}>
                 {navlinks.map(link => {
                     return (
                         <a href={`/#${link['name'].toLowerCase()}`} className={link['classes']} onClick={() => {
