b03add4d50450851d0076503b4eb73cdf0c727c7
SK <sarthakkarandikar03@gmail.com>
2023-05-06 06:23:40 +0000

    May 6, 2023, 11:53 AM

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
       const entry = entries[0]
       console.log(entry.isIntersecting)
     })
-    observer.observe(aboutRef.current,)
+    observer.observe(headerRef.current, aboutRef)
   },[])
   
 
