fd45446b4fcc92f0036f6fa57ba2ae728e69f7b2
SK <sarthakkarandikar03@gmail.com>
2023-05-06 05:09:24 +0000

    May 6, 2023, 10:39 AM

--- components/About.js
+++ components/About.js
@@ -1,6 +1,5 @@
 import styles from '@styles/About.module.css'
 import Image from 'next/image'
-import Link from 'next/link'
 import { useRef } from 'react'
 import useDownloader from 'react-use-downloader'
 
