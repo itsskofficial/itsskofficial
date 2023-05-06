88767ff72601f589268ddc339f6b3c1d4613a311
SK <sarthakkarandikar03@gmail.com>
2023-05-06 07:11:10 +0000

    May 6, 2023, 12:41 PM

--- components/NavBar.js
+++ components/NavBar.js
@@ -41,7 +41,7 @@ const NavBar = (props) => {
     )
 
     console.log(initialNavlinks)
-    const [navlinks, setNavlinks] = useState(initialNavlinks)
+    const [navlinks, setNavlinks] = useState()
 
     useEffect(() => {
         setNavlinks(initialNavlinks)
