ef4954036fb90a124602fa0b4abe79881809be2b
SK <sarthakkarandikar03@gmail.com>
2023-05-06 13:15:29 +0000

    May 6, 2023, 6:45 PM

--- components/Contact.js
+++ components/Contact.js
@@ -35,16 +35,16 @@ const Contact = forwardRef((props,ref) => {
                     Contact
                 </h2>
                 <div className={styles.contactIcons}>
-                <Link href='https://linkedin.com/'>
-                    <i class='fa-brands fa-linkedin fa-3x'/>
-                </Link>
-                <Link href='mailto:sarthakkarandikar03@gmail.com'>
-                    <i class='fa-solid fa-envelope fa-3x'/>
-                </Link>
-                <Link href='tel:+918275017823'>
-                    <i class='fa-solid fa-phone fa-3x'/>
-                </Link>
-            </div>
+                    <Link href='https://linkedin.com/'>
+                        <i class='fa-brands fa-linkedin fa-3x'/>
+                    </Link>
+                    <Link href='mailto:sarthakkarandikar03@gmail.com'>
+                        <i class='fa-solid fa-envelope fa-3x'/>
+                    </Link>
+                    <Link href='tel:+918275017823'>
+                        <i class='fa-solid fa-phone fa-3x'/>
+                    </Link>
+                </div>
                 <h3 className={styles.contactInfo}>
                     I am available on LinkedIn for discussions and stuff. If LinkedIn is not convenient you can mail me too
                 </h3>
