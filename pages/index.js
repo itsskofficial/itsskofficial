3d298714e2cc6efc4e4cd6c1eaae93639fe47b0c
SK <sarthakkarandikar03@gmail.com>
2023-05-06 06:07:39 +0000

    May 6, 2023, 11:37 AM

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
   const contactRef = createRef()
   const refs = [headerRef, aboutRef, skillsRef, contactRef]
 
-    window.addEventListener('scroll', () => {
+    Window.addEventListener('scroll', () => {
       const activeRef = refs.filter((ref) => {
         window.pageYOffset >= ref.current.offsetTop
       })
