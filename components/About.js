5666b908da8e2b47e3a64ecb70b836fb12c242b8
SK <sarthakkarandikar03@gmail.com>
2023-05-06 05:09:32 +0000

    May 6, 2023, 10:39 AM

--- components/About.js
+++ components/About.js
@@ -5,7 +5,6 @@ import useDownloader from 'react-use-downloader'
 
 const About = () => {
     const aboutRef = useRef()
-    console.log(aboutRef.current.offsetTop)
     const {download} = useDownloader()
     return (
         <section ref={aboutRef} id='about' className={styles.parent}>
