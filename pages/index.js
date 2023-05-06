0f975c9b09dcb8b6b7ee545dd1d4c7581d6d01b6
SK <sarthakkarandikar03@gmail.com>
2023-05-06 05:28:18 +0000

    May 6, 2023, 10:58 AM

--- pages/index.js
+++ pages/index.js
@@ -17,11 +17,11 @@ export default function Home() {
   const refs = [headerRef, aboutRef, skillsRef, contactRef]
 
   useEffect(() => {
-    window.addEventListener('scroll', () => {
-      const activeRef = refs.filter((ref) => {
-        window.pageYOffset >= ref.current.offsetTop
-      })
-      setActiveSection(activeRef.current.id)
+    // window.addEventListener('scroll', () => {
+    //   const activeRef = refs.filter((ref) => {
+    //     window.pageYOffset >= ref.current.offsetTop
+    //   })
+    //   setActiveSection(activeRef.current.id)
     })
   }, [])
 
