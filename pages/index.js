ade91260ca378984805ba389718373523dd629c7
SK <sarthakkarandikar03@gmail.com>
2023-05-06 05:19:55 +0000

    May 6, 2023, 10:49 AM

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
       </Head>
       <main>
         <NavBar activeSection={activeSection} />
-        <Header getOffset={ headerRef } />
+        <Header getOffset={  } />
         <About ref={ aboutRef } />
         <Skills ref={ skillsRef } />
         <Contact ref={ contactRef } />
