bf3510f6aa8290e3a2cc9a7d0afaa7ac877df72b
SK <sarthakkarandikar03@gmail.com>
2023-05-06 13:37:42 +0000

    May 6, 2023, 7:07 PM

--- components/NavBar.js
+++ components/NavBar.js
@@ -10,7 +10,7 @@ const NavBar = (props) => {
     const remainingLetters = tempActiveSection.slice(1)
     const activeSection = firstLetterCap + remainingLetters
     const mediaMatch = useMediaQuery('(min-width:1201px)')
-    const [isMenuOpen, setIsMenuOpen] = useState()
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
 
     const tempNavlinks = [
         {
