dbf5661361a9e6e5c6d8ef77bdf87eaa3339ecf1
SK <sarthakkarandikar03@gmail.com>
2023-05-06 03:29:40 +0000

    May 6, 2023, 8:59 AM

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,11 @@ import About from '@components/About'
 import Skills from '@components/Skills'
 import Contact from '@components/Contact'
 import Footer from '@components/Footer'
+import { useRef } from 'react'
 
 export default function Home() {
 
-  const aboutRef = 
+  const aboutRef = useRef()
   return (
     <>
       <Head>
