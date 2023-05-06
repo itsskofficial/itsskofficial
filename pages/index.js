3aba36ed79a11e1dd0683d0a0a56b31d721139df
SK <sarthakkarandikar03@gmail.com>
2023-05-06 06:30:47 +0000

    May 6, 2023, 12:00 PM

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
     // }
 
     const observer = new IntersectionObserver((entries) => {
-      const  = entries.find(entry)
+      const intersectedEntry = entries.find(entry => )
       console.log(entry)
     })
     observer.observe(headerRef.current, aboutRef.current, skillsRef.current, contactRef.current)
