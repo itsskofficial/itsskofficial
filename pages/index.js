fab8bc76b9a48e5acc6ddde458ecb7ff96a0b6bc
SK <sarthakkarandikar03@gmail.com>
2023-05-06 05:24:05 +0000

    May 6, 2023, 10:54 AM

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import { useRef, useState, useEffect } from 'react'
 export default function Home() {
 
   const [activeSection, setActiveSection] = useState('home')
-  const headerRef = useRef()
+  const headerRef = createRef()
   const aboutRef = useRef()
   const skillsRef = useRef()
   const contactRef = useRef()
