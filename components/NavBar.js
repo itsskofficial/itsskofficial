27dcaae0b4617fea885102dfdfd56f7b459ecc8e
SK <sarthakkarandikar03@gmail.com>
2023-05-06 11:45:05 +0000

    May 6, 2023, 5:15 PM

--- components/NavBar.js
+++ components/NavBar.js
@@ -90,48 +90,48 @@ const NavBar = (props) => {
 </div>
 
     smallScreen = 
-    <div className={styles.parent}>
-    <h2 className={styles.navbarLogo}>
-        SK
-    </h2>
-    <ul className={styles.navList}>
-        {navlinks.map(link => {
-            return(
-                <a href={`/#${link['name'].toLowerCase()}`} className={link['classes']} onClick={() => {
-                    var tempNavlinks = [
-                        {
-                            id:0,
-                            name: "Home",
-                            classes: [styles.navLink]
-                        },
-                        {
-                            id:1,
-                            name: "About",
-                            classes: [styles.navLink]
-                        },
-                        {
-                            id:2,
-                            name: "Skills",
-                            classes: [styles.navLink]
-                        },
-                        {
-                            id:3,
-                            name: "Contact",
-                            classes: [styles.navLink]
-                        }
-                    ]
-                    tempNavlinks[link['id']]['classes'] = [styles.navLink, styles.navLinkActive].join(' ')
-                    setNavlinks(tempNavlinks)
-                }}>
-                    {link['name']}
-                </a>
-            )
-        })}
-    </ul>
-    <h2 className={styles.logo}>
-        Mode
-    </h2>
-</div>
+        <div className={styles.parent}>
+        <h2 className={styles.navbarLogo}>
+            SK
+        </h2>
+        <ul className={styles.navList}>
+            {navlinks.map(link => {
+                return(
+                    <a href={`/#${link['name'].toLowerCase()}`} className={link['classes']} onClick={() => {
+                        var tempNavlinks = [
+                            {
+                                id:0,
+                                name: "Home",
+                                classes: [styles.navLink]
+                            },
+                            {
+                                id:1,
+                                name: "About",
+                                classes: [styles.navLink]
+                            },
+                            {
+                                id:2,
+                                name: "Skills",
+                                classes: [styles.navLink]
+                            },
+                            {
+                                id:3,
+                                name: "Contact",
+                                classes: [styles.navLink]
+                            }
+                        ]
+                        tempNavlinks[link['id']]['classes'] = [styles.navLink, styles.navLinkActive].join(' ')
+                        setNavlinks(tempNavlinks)
+                    }}>
+                        {link['name']}
+                    </a>
+                )
+            })}
+        </ul>
+        <h2 className={styles.logo}>
+            Mode
+        </h2>
+    </div>
 
 
     return (
