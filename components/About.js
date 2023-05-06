49e0f409851b82bde5a28210fb1b61932d43d8f2
SK <sarthakkarandikar03@gmail.com>
2023-05-06 06:50:39 +0000

    May 6, 2023, 12:20 PM

--- components/About.js
+++ components/About.js
@@ -7,7 +7,7 @@ const About = forwardRef((props,ref) => {
     const aboutRef = useRef()
     const { download } = useDownloader()
     return (
-        <section ref={ref} onMouseEnter={printOffset} id='about' className={styles.parent}>
+        <section ref={ref} id='about' className={styles.parent}>
             <div className={styles.aboutImage}>
                 <Image src='/../public/assets/images/aboutImage.jpeg' width='400' height='500' alt='About Image' />
             </div>
