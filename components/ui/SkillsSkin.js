ae4ac3f1d49b799702580d15d90c9fa7efdf0f7a
SK <sarthakkarandikar03@gmail.com>
2023-05-07 07:39:41 +0000

    May 7, 2023, 1:09 PM

--- components/ui/SkillsSkin.js
+++ components/ui/SkillsSkin.js
@@ -13,7 +13,7 @@ const SkillsSkin = (props) => {
                 <i class={`fa-brands fa-js fa-${mediaMatch?'3x':'2x'}`} />
                 <i class={`fa-brands fa-c fa-${mediaMatch?'3x':'2x'}`} />
                 <i class={`fa-brands fa-html5 fa-${mediaMatch? '3x': '2x' }`} />
-                <Image src='/assets/images/tf.svg' width='60' height='60' alt='TF Icon'/>
+                
             </div>
             <div className={styles.skillsSkinRow} >
                 <i class={`fa-brands fa-css3 fa-${mediaMatch?'3x':'2x'}`} />
@@ -22,7 +22,8 @@ const SkillsSkin = (props) => {
             </div>
             <div className = {styles.skillsSkinRow } >
                 <i class={`fa-brands fa-react fa-${mediaMatch?'3x':'2x'}`} />
-                <i class={`fa-brands fa-node fa-${mediaMatch?'3x':'2x'}`} />
+                <i class={`fa-brands fa-node fa-${mediaMatch ? '3x' : '2x'}`} />
+                <Image src='/assets/images/tf.svg' width='60' height='60' alt='TF Icon'/>
             </div>
         </Fragment>
             
