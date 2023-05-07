97ccbbb06e89f6fc351849d4853dbdef5d8852c4
SK <sarthakkarandikar03@gmail.com>
2023-05-07 09:01:32 +0000

    May 7, 2023, 2:31 PM

--- components/ui/SkillsSkin.js
+++ components/ui/SkillsSkin.js
@@ -57,8 +57,13 @@ const SkillsSkin = (props) => {
                 <i class={`fa-brands fa-slack fa-${mediaMatch ? '3x' : '2x'}`} />
                 <i class={`fa-brands fa-jira fa-${mediaMatch ? '3x' : '2x'}`} />
                 <i class={`fa-brands fa-confluence fa-${mediaMatch ? '3x' : '2x'}`} />
-                <Image src='/assets/images/davinci.svg' width='60' height='60' alt='TF Icon'/>
-                <Image src='/assets/images/trello.svg' width='60' height='60' alt='TF Icon'/>
+            </div>
+            <div className={styles.skillsSkinRow}>
+                <Image src='/assets/images/asana.svg' width='60' height='60' alt='TF Icon' />
+                <Image src='/assets/images/gimp.svg' width='60' height='60' alt='TF Icon'/>
+                <i class={`fa-brands fa-slack fa-${mediaMatch ? '3x' : '2x'}`} />
+                <i class={`fa-brands fa-jira fa-${mediaMatch ? '3x' : '2x'}`} />
+                <i class={`fa-brands fa-confluence fa-${mediaMatch ? '3x' : '2x'}`} />
             </div>
         </Fragment>
         )
