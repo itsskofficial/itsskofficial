650fea16cbbc01767c9cdca5a6d72dfb95b0c22f
SK <sarthakkarandikar03@gmail.com>
2023-05-07 06:28:19 +0000

    May 7, 2023, 11:58 AM

--- components/About.js
+++ components/About.js
@@ -10,7 +10,7 @@ const About = forwardRef((props, ref) => {
     return (
         <section ref={ref} id='about' className={styles.parent}>
             <div className={styles.aboutImage}>
-                <Image src='/../public/assets/images/aboutImage.jpeg' width={mediaMatch?'400':'300'} height='350' alt='About Image' />
+                <Image src='/../public/assets/images/aboutImage.jpeg' width={mediaMatch?'400':'300'} height={mediaMatch?'500':'350'} alt='About Image' />
             </div>
             <div className={styles.aboutText}>
                 <h1 className={styles.aboutTitle}>
