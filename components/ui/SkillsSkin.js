fb1fa499d94607dcc89bb3318ba3ae70f8d51445
SK <sarthakkarandikar03@gmail.com>
2023-05-07 08:25:01 +0000

    May 7, 2023, 1:55 PM

--- components/ui/SkillsSkin.js
+++ components/ui/SkillsSkin.js
@@ -28,6 +28,7 @@ const SkillsSkin = (props) => {
                 <Image src='/assets/images/nextjs.svg' width='60' height='60' alt='NextJS Icon'/>
                 <i class={`fa-brands fa-node fa-${mediaMatch ? '3x' : '2x'}`} />
                 <Image src='/assets/images/tf.svg' width='60' height='60' alt='TF Icon'/>
+                <Image src='/assets/images/लहसजब.svg' width='60' height='60' alt='TF Icon'/>
             </div>
         </Fragment>
             
