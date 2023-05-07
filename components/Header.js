fdfcf55554b97d11b2a1a10f495bc77831e0069e
SK <sarthakkarandikar03@gmail.com>
2023-05-07 06:28:45 +0000

    May 7, 2023, 11:58 AM

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ import { TypeAnimation } from 'react-type-animation'
 import { useMediaQuery } from 'usehooks-ts'
 
 const Header = forwardRef((props,ref) => {
-    const mediaMatch = useMediaQuery(('min-width:'))
+    const mediaMatch = useMediaQuery(('min-width:1201px'))
     const roles = ["AI", "IOT", "Metaverse"]
     return (
         <section ref={ref} id='home' className={styles.parent}>
