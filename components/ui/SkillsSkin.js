800371455cd12265351bbdef0029036041f8fb8a
SK <sarthakkarandikar03@gmail.com>
2023-05-07 08:57:07 +0000

    May 7, 2023, 2:27 PM

--- components/ui/SkillsSkin.js
+++ components/ui/SkillsSkin.js
@@ -45,7 +45,7 @@ const SkillsSkin = (props) => {
     const toolsIcons = (
             <div className = {styles.skillsSkinRow }>
             <i class={`fa-brands fa-figma fa-${mediaMatch ? '3x' : '2x'}`} />
-            <Image src='/assets/images/numpy.svg' width='60' height='60' alt='TF Icon'/>
+            <Image src='/assets/images/canva.svg' width='60' height='60' alt='TF Icon'/>
 
             </div>
         )
