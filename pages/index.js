66fdd70c02681a4e87abec6ac559ec516f298a97
SK <sarthakkarandikar03@gmail.com>
2023-05-08 01:30:53 +0000

    May 8, 2023, 7:00 AM

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ export default function Home() {
         <Contact ref={ contactRef } />
         <Footer />
         <Script src='https://kit.fontawesome.com/638bbcf842.js' crossorigin='anonymous' />
-        {AOS.init()}
+        AOS.init()
       </main>
     </>
   )
