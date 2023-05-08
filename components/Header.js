58d7c5188a435415281da2f3769b11d12595004b
SK <sarthakkarandikar03@gmail.com>
2023-05-08 01:18:05 +0000

    May 8, 2023, 6:48 AM

--- components/Header.js
+++ components/Header.js
@@ -42,7 +42,7 @@ const Header = forwardRef((props, ref) => {
                 </button>
             </div>
             <div className={styles.headImage}>
-                <Image src='/../public/assets/images/headImage.jpg' style={{width:mediaMatch?}} priority={true} width={mediaMatch ? '500' : '300'} height={mediaMatch ? '500' : '300'} alt='Hero Image'/>
+                <Image src='/../public/assets/images/headImage.jpg' style={{width:mediaMatch?'500':'300'}} priority={true} width={mediaMatch ? '500' : '300'} height={mediaMatch ? '500' : '300'} alt='Hero Image'/>
             </div>
         </section>
         
