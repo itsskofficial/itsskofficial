2e5fb1a3d21300f87bdd5bed5ff6722ce05957ba
SK <sarthakkarandikar03@gmail.com>
2023-05-08 00:16:41 +0000

    May 8, 2023, 5:46 AM

--- components/Skills.js
+++ components/Skills.js
@@ -7,7 +7,6 @@ const Skills = forwardRef((props,ref) => {
     const [skillsOption, setSkillsOption] = useState('code')
     const [optionChanged, setOptionChanged] = useState(false)
     const mediaMatch = useMediaQuery('(min-width:1201px)')
-    console.log(mediaMatch)
 
     useEffect(() => {
         if (optionChanged) {
