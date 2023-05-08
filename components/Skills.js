5f4725f1cbf7f7ec768d8a067122e163cae2717d
SK <sarthakkarandikar03@gmail.com>
2023-05-08 01:10:14 +0000

    May 8, 2023, 6:40 AM

--- components/Skills.js
+++ components/Skills.js
@@ -68,7 +68,7 @@ const Skills = forwardRef((props,ref) => {
                             setOptionChanged(true)
                         }   
                     }}>
-                        <i class='fa-solid fa-code' style={{marginRight: '10px'}} />Code
+                        <i className='fa-solid fa-code' style={{marginRight: '10px'}} />Code
                     </button>
                     <button className={[styles.skillsTools,skillsOption=='tools'?styles.skillsActiveButton:null].join(' ')} onClick={() => {
                         if (skillsOption != 'tools') {
@@ -76,7 +76,7 @@ const Skills = forwardRef((props,ref) => {
                             setOptionChanged(true)
                         }
                     }}>
-                        <i class='fa-solid fa-screwdriver-wrench' style={{marginRight: '10px'}} />Tools
+                        <i className='fa-solid fa-screwdriver-wrench' style={{marginRight: '10px'}} />Tools
                     </button>
                 </div>
             </div>
