24a9932c4a2e72df4f1bde54dada9512baed5285
SK <sarthakkarandikar03@gmail.com>
2023-05-08 01:04:18 +0000

    May 8, 2023, 6:34 AM

--- components/ui/SkillsSkin.js
+++ components/ui/SkillsSkin.js
@@ -16,11 +16,11 @@ const SkillsSkin = (props) => {
     const codeIcons = (
         <Fragment>
             <div className={styles.skillsSkinRow} >
-                <i class={`fa-brands fa-python fa-${mediaMatch?'3x':'2x'}`} />
-                <i class={`fa-brands fa-js fa-${mediaMatch?'3x':'2x'}`} />
-                <i class={`fa-brands fa-c fa-${mediaMatch?'3x':'2x'}`} />
+                <i className={`fa-brands fa-python fa-${mediaMatch?'3x':'2x'}`} />
+                <i classclassName={`fa-brands fa-js fa-${mediaMatch?'3x':'2x'}`} />
+                <i classclassName={`fa-brands fa-c fa-${mediaMatch?'3x':'2x'}`} />
                 <Image src='/assets/images/cpp.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='C++ Icon'/>
-                <i class={`fa-brands fa-html5 fa-${mediaMatch ? '3x' : '2x'}`} />
+                <i className={`fa-brands fa-html5 fa-${mediaMatch ? '3x' : '2x'}`} />
                 
                 
             </div>
