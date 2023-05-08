3aae90732e41af0002c443099e4cc7ce3de1418a
SK <sarthakkarandikar03@gmail.com>
2023-05-08 00:13:39 +0000

    May 8, 2023, 5:43 AM

--- components/ui/SkillsSkin.js
+++ components/ui/SkillsSkin.js
@@ -32,7 +32,7 @@ const SkillsSkin = (props) => {
                 <Image src='/assets/images/pandas.svg' width='60' height='60' alt='Pandas Icon'/>
             </div>
             <div className = {styles.skillsSkinRow } >
-                <Image src='/assets/images/sklearn.png' width='60' height='60' alt='ScikitLearn Icon'/>
+                {/* <Image src='/assets/images/sklearn.png' width='60' height='60' alt='ScikitLearn Icon'/> */}
                 <i class={`fa-brands fa-ethereum fa-${mediaMatch ? '3x' : '2x'}`} />
                 <Image src='/assets/images/solidity.svg' width='60' height='60' alt='Solidity Icon'/>
                 <Image src='/assets/images/arduino.svg' width='60' height='60' alt='Arduino Icon'/>
