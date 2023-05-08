54cb02770f9a18cc413f997589cc825447188300
SK <sarthakkarandikar03@gmail.com>
2023-05-08 00:15:06 +0000

    May 8, 2023, 5:45 AM

--- components/ui/SkillsSkin.js
+++ components/ui/SkillsSkin.js
@@ -12,7 +12,7 @@ const SkillsSkin = (props) => {
                 <i class={`fa-brands fa-python fa-${mediaMatch?'3x':'2x'}`} />
                 <i class={`fa-brands fa-js fa-${mediaMatch?'3x':'2x'}`} />
                 <i class={`fa-brands fa-c fa-${mediaMatch?'3x':'2x'}`} />
-                <Image src='/assets/images/cpp.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='C++ Icon'/>
+                <Image src='/assets/images/cpp.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='C++ Icon'/>
                 <i class={`fa-brands fa-html5 fa-${mediaMatch ? '3x' : '2x'}`} />
                 
                 
@@ -21,22 +21,22 @@ const SkillsSkin = (props) => {
                 <i class={`fa-brands fa-css3 fa-${mediaMatch?'3x':'2x'}`} />
                 <i class={`fa-brands fa-react fa-${mediaMatch?'3x':'2x'}`} />
                 <i class={`fa-brands fa-node fa-${mediaMatch ? '3x' : '2x'}`} />
-                <Image src='/assets/images/ejs.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='EJS Icon'/>
-                <Image src='/assets/images/mongodb.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='MongoDB Icon'/>
+                <Image src='/assets/images/ejs.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='EJS Icon'/>
+                <Image src='/assets/images/mongodb.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='MongoDB Icon'/>
             </div>
             <div className = {styles.skillsSkinRow } >
-                <Image src='/assets/images/nextjs.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='NextJS Icon'/>
+                <Image src='/assets/images/nextjs.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='NextJS Icon'/>
                 <i class={`fa-brands fa-node fa-${mediaMatch ? '3x' : '2x'}`} />
-                <Image src='/assets/images/tf.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='TF Icon'/>
-                <Image src='/assets/images/numpy.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='Numpy Icon'/>
-                <Image src='/assets/images/pandas.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='Pandas Icon'/>
+                <Image src='/assets/images/tf.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='TF Icon'/>
+                <Image src='/assets/images/numpy.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='Numpy Icon'/>
+                <Image src='/assets/images/pandas.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='Pandas Icon'/>
             </div>
             <div className = {styles.skillsSkinRow } >
-                {/* <Image src='/assets/images/sklearn.png' width={mediaMatch'60'} height={mediaMatch'60'} alt='ScikitLearn Icon'/> */}
+                {/* <Image src='/assets/images/sklearn.png' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='ScikitLearn Icon'/> */}
                 <i class={`fa-brands fa-ethereum fa-${mediaMatch ? '3x' : '2x'}`} />
-                <Image src='/assets/images/solidity.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='Solidity Icon'/>
-                <Image src='/assets/images/arduino.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='Arduino Icon'/>
-                {/* <Image src='/assets/images/pandas.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='Pandas Icon'/> */}
+                <Image src='/assets/images/solidity.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='Solidity Icon'/>
+                <Image src='/assets/images/arduino.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='Arduino Icon'/>
+                {/* <Image src='/assets/images/pandas.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='Pandas Icon'/> */}
             </div>
         </Fragment>
             
@@ -46,30 +46,30 @@ const SkillsSkin = (props) => {
         <Fragment>
             <div className = {styles.skillsSkinRow }>
                 <i class={`fa-brands fa-figma fa-${mediaMatch ? '3x' : '2x'}`} />
-                <Image src='/assets/images/canva.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='Canva Icon'/>
-                <Image src='/assets/images/audacity.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='Audacity Icon'/>
-                <Image src='/assets/images/davinci.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='Davinci Icon'/>
-                <Image src='/assets/images/trello.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='Trello Icon'/>
+                <Image src='/assets/images/canva.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='Canva Icon'/>
+                <Image src='/assets/images/audacity.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='Audacity Icon'/>
+                <Image src='/assets/images/davinci.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='Davinci Icon'/>
+                <Image src='/assets/images/trello.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='Trello Icon'/>
             </div>
             <div className={styles.skillsSkinRow}>
-                <Image src='/assets/images/asana.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='Asana Icon' />
-                <Image src='/assets/images/gimp.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='Gimp Icon'/>
+                <Image src='/assets/images/asana.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='Asana Icon' />
+                <Image src='/assets/images/gimp.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='Gimp Icon'/>
                 <i class={`fa-brands fa-slack fa-${mediaMatch ? '3x' : '2x'}`} />
                 <i class={`fa-brands fa-jira fa-${mediaMatch ? '3x' : '2x'}`} />
                 <i class={`fa-brands fa-confluence fa-${mediaMatch ? '3x' : '2x'}`} />
             </div>
             <div className={styles.skillsSkinRow}>
                 <i class={`fa-brands fa-wordpress fa-${mediaMatch ? '3x' : '2x'}`} />
-                <Image src='/assets/images/lmms.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='LMMS Icon' />
-                <Image src='/assets/images/inkscape.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='Inkscape Icon'/>
-                <Image src='/assets/images/vscode.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='VSCode Icon'/>
+                <Image src='/assets/images/lmms.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='LMMS Icon' />
+                <Image src='/assets/images/inkscape.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='Inkscape Icon'/>
+                <Image src='/assets/images/vscode.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='VSCode Icon'/>
                 <i class={`fa-brands fa-github fa-${mediaMatch ? '3x' : '2x'}`} />
             </div>
             {/* <div className={styles.skillsSkinRow}>
                 <i class={`fa-brands fa-trello fa-${mediaMatch ? '3x' : '2x'}`} />
-                {/* <Image src='/assets/images/lmms.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='TF Icon' />
-                <Image src='/assets/images/inkscape.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='TF Icon'/>
-                <Image src='/assets/images/vscode.svg' width={mediaMatch'60'} height={mediaMatch'60'} alt='TF Icon'/>
+                {/* <Image src='/assets/images/lmms.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='TF Icon' />
+                <Image src='/assets/images/inkscape.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='TF Icon'/>
+                <Image src='/assets/images/vscode.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='TF Icon'/>
                 <i class={`fa-brands fa-github fa-${mediaMatch ? '3x' : '2x'}`} /> */} 
             {/* </div> */}
         </Fragment>
