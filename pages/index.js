2995d67619e179d21e7da462f3482542c90ee682
SK <sarthakkarandikar03@gmail.com>
2023-05-10 15:52:57 +0000

    May 10, 2023, 9:22 PM

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Helmet >
+        <Helmet style={}>
           <NavBar activeSection={activeSection} mode={mode} toggleMode={toggleMode} />
           <Header ref={ headerRef} mode={mode}/>
           <About ref={ aboutRef } mode={mode}/>
