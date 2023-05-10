2966908add1169212f33b2e8b068d8845a3d8f19
SK <sarthakkarandikar03@gmail.com>
2023-05-10 09:40:25 +0000

    May 10, 2023, 3:10 PM

--- components/NavBar.js
+++ components/NavBar.js
@@ -110,6 +110,7 @@ const NavBar = (props) => {
                     setMode('dark')
                 else
                     setMode('light')
+                get
             }}>
                 <i className={`fa-solid fa-${mode == 'dark' ? 'moon' : 'sun'}`} />
             </span>
