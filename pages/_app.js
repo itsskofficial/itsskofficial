fea26bc4ef24a2899ebab1519af51bbd1c953b23
SK <sarthakkarandikar03@gmail.com>
2023-05-10 07:44:48 +0000

    May 10, 2023, 1:14 PM

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@styles/globals.css'
-
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 
 export default function App({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,6 @@ import Contact from '@components/Contact'
 import Footer from '@components/Footer'
 import { useState, useEffect, createRef } from 'react'
 import Script from 'next/script'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
 
 export default function Home() {
 
