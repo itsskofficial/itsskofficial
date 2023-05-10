8ba961d971377ceda6a5e191734777ce99be6f05
SK <sarthakkarandikar03@gmail.com>
2023-05-10 14:59:55 +0000

    May 10, 2023, 8:29 PM

--- components/NavBar.js
+++ components/NavBar.js
@@ -53,7 +53,7 @@ const NavBar = (props) => {
     const initialNavlinks = tempNavlinks.map(link => link['name'] == activeSection ? {
         id: link['id'],
         name: link['name'],
-        classes: [props.mode=='dark'styles.navLink, styles.navLinkActive,].join(' ')
+        classes: [props.mode=='dark'?styles.navLink:s, styles.navLinkActive,].join(' ')
     } :
         link
     )
