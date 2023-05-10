18574b93ece91967d90c23cae682629eddc3f4fc
SK <sarthakkarandikar03@gmail.com>
2023-05-10 07:44:22 +0000

    May 10, 2023, 1:14 PM

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '@styles/globals.css'
 
+
+
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init();
+  }, [])
   return <Component {...pageProps} />
 }
