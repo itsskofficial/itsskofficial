8d7991c5c7d6ec745251581c1bb8df3f2cd6a4ad
SK <sarthakkarandikar03@gmail.com>
2023-05-10 15:11:24 +0000

    May 10, 2023, 8:41 PM

--- components/Header.js
+++ components/Header.js
@@ -35,7 +35,7 @@ const Header = forwardRef((props, ref) => {
                         deletionSpeed={20} />
                     developer
                 </h2>
-                <button className={props.mode=='dark'?tyles.headContact:styles.headContactLights}>
+                <button className={props.mode=='dark'?tyles.headContact:styles.headContactLight}>
                     <a href='#contact'>
                         <i className='fa-solid fa-message' style={{marginRight: '10px'}} />Contact Me
                     </a>
