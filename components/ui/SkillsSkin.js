41f2e246c8454fd29b6f69c0690700711ab2f4de
SK <sarthakkarandikar03@gmail.com>
2023-05-11 06:48:28 +0000

    May 11, 2023, 12:18 PM

--- components/ui/SkillsSkin.js
+++ components/ui/SkillsSkin.js
@@ -16,16 +16,16 @@ const SkillsSkin = (props) => {
     const codeIcons = (
         <Fragment>
             <div className={styles.skillsSkinRow} style={{color:props.mode=='dark'?'white': '#171717'}}>
-                <i className={`fa-brands fa-python fa-${mediaMatch?'3x':'2x'}`} style={}/>
-                <i className={`fa-brands fa-js fa-${mediaMatch?'3x':'2x'}`} style={{color:props.mode=='dark'?null: '#171717'}}/>
-                <i className={`fa-brands fa-c fa-${mediaMatch?'3x':'2x'}`} style={{color:props.mode=='dark'?null: '#171717'}}/>
+                <i className={`fa-brands fa-python fa-${mediaMatch?'3x':'2x'}`} />
+                <i className={`fa-brands fa-js fa-${mediaMatch?'3x':'2x'}`} />
+                <i className={`fa-brands fa-c fa-${mediaMatch?'3x':'2x'}`} />
                 <Image src='/assets/images/cpp.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='C++ Icon'/>
-                <i className={`fa-brands fa-html5 fa-${mediaMatch ? '3x' : '2x'}`} style={{color:props.mode=='dark'?null: '#171717'}}/>
+                <i className={`fa-brands fa-html5 fa-${mediaMatch ? '3x' : '2x'}`} />
             </div>
             <div className={styles.skillsSkinRow} >
-                <i className={`fa-brands fa-css3 fa-${mediaMatch?'3x':'2x'}`} style={{color:props.mode=='dark'?null: '#171717'}}/>
-                <i className={`fa-brands fa-react fa-${mediaMatch?'3x':'2x'}`} style={{color:props.mode=='dark'?null: '#171717'}}/>
-                <i className={`fa-brands fa-node fa-${mediaMatch ? '3x' : '2x'}`} style={{color:props.mode=='dark'?null: '#171717'}}/>
+                <i className={`fa-brands fa-css3 fa-${mediaMatch?'3x':'2x'}`} />
+                <i className={`fa-brands fa-react fa-${mediaMatch?'3x':'2x'}`} />
+                <i className={`fa-brands fa-node fa-${mediaMatch ? '3x' : '2x'}`} />
                 <Image src='/assets/images/ejs.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='EJS Icon'/>
                 <Image src='/assets/images/mongodb.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='MongoDB Icon'/>
             </div>
@@ -35,7 +35,7 @@ const SkillsSkin = (props) => {
                 <Image src='/assets/images/tf.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='TF Icon'/>
                 <Image src='/assets/images/numpy.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='Numpy Icon'/>
                 <Image src='/assets/images/pandas.svg' width={mediaMatch ? '60' : '30'} height={mediaMatch ? '60' : '30'} alt='Pandas Icon' />
-                <i className={`fa-brands fa-ethereum fa-${mediaMatch ? '3x' : '2x'}`} style={{color:props.mode=='dark'?null: '#171717'}}/>
+                <i className={`fa-brands fa-ethereum fa-${mediaMatch ? '3x' : '2x'}`} />
             </div>
             <div className = {styles.skillsSkinRow } >
                 {/* <Image src='/assets/images/sklearn.png' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='ScikitLearn Icon'/> */}
@@ -50,7 +50,7 @@ const SkillsSkin = (props) => {
     const toolsIcons = (
         <Fragment>
             <div className = {styles.skillsSkinRow }>
-                <i className={`fa-brands fa-figma fa-${mediaMatch ? '3x' : '2x'}`} style={{color:props.mode=='dark'?null: '#171717'}}/>
+                <i className={`fa-brands fa-figma fa-${mediaMatch ? '3x' : '2x'}`} />
                 <Image src='/assets/images/canva.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='Canva Icon'/>
                 <Image src='/assets/images/audacity.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='Audacity Icon'/>
                 <Image src='/assets/images/davinci.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='Davinci Icon'/>
@@ -59,16 +59,16 @@ const SkillsSkin = (props) => {
             <div className={styles.skillsSkinRow}>
                 <Image src='/assets/images/asana.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='Asana Icon' />
                 <Image src='/assets/images/gimp.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='Gimp Icon'/>
-                <i className={`fa-brands fa-slack fa-${mediaMatch ? '3x' : '2x'}`} style={{color:props.mode=='dark'?null: '#171717'}}/>
-                <i className={`fa-brands fa-jira fa-${mediaMatch ? '3x' : '2x'}`} style={{color:props.mode=='dark'?null: '#171717'}}/>
-                <i className={`fa-brands fa-confluence fa-${mediaMatch ? '3x' : '2x'}`} style={{color:props.mode=='dark'?null: '#171717'}}/>
+                <i className={`fa-brands fa-slack fa-${mediaMatch ? '3x' : '2x'}`} />
+                <i className={`fa-brands fa-jira fa-${mediaMatch ? '3x' : '2x'}`} />
+                <i className={`fa-brands fa-confluence fa-${mediaMatch ? '3x' : '2x'}`} />
             </div>
             <div className={styles.skillsSkinRow}>
-                <i className={`fa-brands fa-wordpress fa-${mediaMatch ? '3x' : '2x'}`} style={{color:props.mode=='dark'?null: '#171717'}}/>
+                <i className={`fa-brands fa-wordpress fa-${mediaMatch ? '3x' : '2x'}`} />
                 <Image src='/assets/images/lmms.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='LMMS Icon' />
                 <Image src='/assets/images/inkscape.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='Inkscape Icon'/>
                 <Image src='/assets/images/vscode.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='VSCode Icon'/>
-                <i className={`fa-brands fa-github fa-${mediaMatch ? '3x' : '2x'}`} style={{color:props.mode=='dark'?null: '#171717'}}/>
+                <i className={`fa-brands fa-github fa-${mediaMatch ? '3x' : '2x'}`} />
             </div>
             {/* <div className={styles.skillsSkinRow}>
                 <i class={`fa-brands fa-trello fa-${mediaMatch ? '3x' : '2x'}`} />
