4e9e3f509d60aa3be50f93971452438496621f2f
SK <sarthakkarandikar03@gmail.com>
2023-05-11 00:26:16 +0000

    May 11, 2023, 5:56 AM

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ export default function Home() {
     if (mode == 'dark')
       document.body.style.backgroundColor = '#171717'
     else
-    document.body.style.backgroundColor = '#171717'
+      document.body.style.backgroundColor = '#ffffff'
   },[])
   
 
