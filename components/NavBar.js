eae9abb5225ca91efa15f696e99764fdb4b342fa
SK <sarthakkarandikar03@gmail.com>
2023-05-11 00:40:06 +0000

    May 11, 2023, 6:10 AM

--- components/NavBar.js
+++ components/NavBar.js
@@ -73,7 +73,7 @@ const NavBar = (props) => {
     }, [activeSection])
     
     const bigScreen = (
-        <section className={styles.parent}>
+        <section className={props.mode?styles.parent}>
             <h2 className={[styles.navbarLogo,isDarkMode?null:styles.light].join(' ')}>
                 SK
             </h2>
