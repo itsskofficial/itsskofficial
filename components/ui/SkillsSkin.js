ce969dfcce3312f33649770580e5fa23a2ed6fd8
SK <sarthakkarandikar03@gmail.com>
2023-05-11 00:04:13 +0000

    May 11, 2023, 5:34 AM

--- components/ui/SkillsSkin.js
+++ components/ui/SkillsSkin.js
@@ -20,7 +20,7 @@ const SkillsSkin = (props) => {
                 <i className={`fa-brands fa-js fa-${mediaMatch?'3x':'2x'}`} />
                 <i className={`fa-brands fa-c fa-${mediaMatch?'3x':'2x'}`} />
                 <Image src='/assets/images/cpp.svg' width={mediaMatch?'60':'30'} height={mediaMatch?'60':'30'} alt='C++ Icon'/>
-                <i className={`fa-brands fa-html5 fa-${mediaMatch ? '3x' : '2x'}`} />
+                <i className={`fa-brands fa-html5 fa-${mediaMatch ? '3x' : '2x'}`} style={{color:props.mode=='light' && '#171717'}}/>
                 
                 
             </div>
