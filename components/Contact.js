25ad176405446182fe7ed95b39958994795865f3
SK <sarthakkarandikar03@gmail.com>
2023-05-11 01:12:53 +0000

    May 11, 2023, 6:42 AM

--- components/Contact.js
+++ components/Contact.js
@@ -11,7 +11,7 @@ const Contact = forwardRef((props, ref) => {
         if (isBigScreen == false){
             mediaMatch=false
         }
-    },[])
+    },[isBigScreen])
 
     const bigScreen =
         <section data-aos='fade-in' ref={ref} id='contact' className={styles.parent}>
