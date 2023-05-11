3f381a3c5e04c72593a43317d8484b58e02e4a22
SK <sarthakkarandikar03@gmail.com>
2023-05-11 03:50:56 +0000

    May 11, 2023, 9:20 AM

--- components/NavBar.js
+++ components/NavBar.js
@@ -17,7 +17,7 @@ const NavBar = (props) => {
         if (isBigScreen == false){
             mediaMatch=false
         }
-        mediaMatch?ma
+        mediaMatch?mediaMatchTemp=
     })
 
     console.log(mediaMatch)
