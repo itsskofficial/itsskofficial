bac971a6352fc23d8aac263ab4916acbb7b9a6b8
SK <sarthakkarandikar03@gmail.com>
2023-05-11 03:47:50 +0000

    May 11, 2023, 9:17 AM

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react"
 import { useMediaQuery } from "usehooks-ts"
 
 const NavBar = (props) => {
-    mediaMatch!='undefined'&&console.log(mediaMatch)
     const tempActiveSection = props.activeSection
     const firstLetter = tempActiveSection[0]
     const firstLetterCap = firstLetter.toUpperCase()
