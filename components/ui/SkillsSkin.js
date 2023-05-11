4281284eeed9af27df4c1cebef9a7550a941a651
SK <sarthakkarandikar03@gmail.com>
2023-05-11 06:47:08 +0000

    May 11, 2023, 12:17 PM

--- components/ui/SkillsSkin.js
+++ components/ui/SkillsSkin.js
@@ -15,7 +15,7 @@ const SkillsSkin = (props) => {
 
     const codeIcons = (
         <Fragment>
-            <div className={styles.skillsSkinRow} {color:props.mode=='dark'?null: '#171717'}}>
+            <div className={styles.skillsSkinRow} style={{color:props.mode=='dark'?null: '#171717'}}>
                 <i className={`fa-brands fa-python fa-${mediaMatch?'3x':'2x'}`} style={}/>
                 <i className={`fa-brands fa-js fa-${mediaMatch?'3x':'2x'}`} style={{color:props.mode=='dark'?null: '#171717'}}/>
                 <i className={`fa-brands fa-c fa-${mediaMatch?'3x':'2x'}`} style={{color:props.mode=='dark'?null: '#171717'}}/>
