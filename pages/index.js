08fac095128e983581ea66cb63499ef632383786
SK <sarthakkarandikar03@gmail.com>
2023-05-11 05:39:17 +0000

    May 11, 2023, 11:09 AM

--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,7 @@ export default function Home() {
         <meta name="description" content="SK's personal portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel='stylesheet' href={mode=='dark'?'styles/menu.css':'styles/menuLight.css'}/>
+        <link rel='stylesheet' href={mode=='dark'?'/styles/menu.css':'/styles/menuLight.css'}/>
       </Head>
         <main>
           <NavBar activeSection={activeSection} mode={mode} toggleMode={toggleMode} />
