2df7e86dc59c17b210b907752ef0a74fcab213ff
SK <sarthakkarandikar03@gmail.com>
2023-05-11 03:51:16 +0000

    May 11, 2023, 9:21 AM

--- components/NavBar.js
+++ components/NavBar.js
@@ -12,6 +12,7 @@ const NavBar = (props) => {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
     const isBigScreen = useMediaQuery('(min-width:1201px)')
     var mediaMatch=true
+    var mediaMatchTemp
 
     useEffect(() => {
         if (isBigScreen == false){
