2ac1b791199de7c464009ab4950cad8f109f39f6
SK <sarthakkarandikar03@gmail.com>
2023-05-11 01:24:07 +0000

    May 11, 2023, 6:54 AM

--- components/Contact.js
+++ components/Contact.js
@@ -37,7 +37,7 @@ const Contact = forwardRef((props, ref) => {
         </section>
 
     const smallScreen =
-        <section ref={ref} id='contact' className={styles.parent}>
+        <section data-aos='fade-in' ref={ref} id='contact' className={styles.parent}>
             <div className={styles.contactText}>
                 <h2 className={[styles.contactTitle, props.mode=='dark'?null:styles.light].join(' ')}>
                     Contact
