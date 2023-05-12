b51d6bb0e37c6d5137dfa74bb0f865c5fb91510f
SK <sarthakkarandikar03@gmail.com>
2023-05-12 04:41:06 +0000

    May 12, 2023, 10:11 AM

--- components/Contact.js
+++ components/Contact.js
@@ -54,7 +54,7 @@ const Contact = forwardRef((props, ref) => {
                     </Link>
                 </div>
                 <h3 className={[styles.contactInfo, props.mode=='dark'?null:styles.light].join(' ')}>
-                    I generally use LinkedIn for expressing myself. You can Whatsapp me for any professional reasons or mail me for discussions and stuff
+                    I generally use LinkedIn for expressing myself. You can WhatsApp me for any professional reasons or mail me for discussions and stuff
                 </h3>
             </div>
         </section>
