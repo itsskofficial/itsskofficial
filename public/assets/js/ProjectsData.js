ea4002c4871deedd3183166e6d10838b19b9a25f
SK <sarthakkarandikar03@gmail.com>
2023-06-15 19:22:08 +0530

    added more projects

--- public/assets/js/ProjectsData.js
+++ public/assets/js/ProjectsData.js
@@ -22,7 +22,79 @@ const projectsData = [
         link: 'https://aesthetic-art.github.io',
         github: 'https://github.com/Aesthetic-Art/Aesthetic-Art.github.io',
         image: '/assets/images/aesthetic-art.png'
-    }
+    },
+    {
+        name: 'Campy',
+        id: 4,
+        description: 'A full stack project which lets you add and rate campgrounds',
+        link: 'https://github.com/itsskofficial/web2/campy/',
+        github: 'https://github.com/itsskofficial/web2/campy/',
+        image: '/assets/images/campy.png'
+    },
+    {
+        name: 'OST Placements',
+        id: 5,
+        description: 'A landing page I made for a customized gifts small business. Made using HTML, CSS & Vanilla JS',
+        link: 'https://blog-landing-lake.vercel.app/',
+        github: 'https://github.com/itsskofficial/web2/ost-placements/',
+        image: '/assets/images/ost-placements.png'
+    },
+    {
+        name: 'Lending Prediction Model',
+        id: 6,
+        description: 'A model to predict the right loan receivers using machine learning',
+        link: 'https://github.com/itsskofficial/Machine-Learning/blob/master/Decision%20Trees%20And%20Random%20Forests/project.ipynb',
+        github: 'https://github.com/itsskofficial/Machine-Learning/blob/master/Decision%20Trees%20And%20Random%20Forests/project.ipynb',
+        image: '/assets/images/lending-prediction.png'
+    },
+    {
+        name: 'Yelp Review Model',
+        id: 7,
+        description: 'A model to predict the rating on Yelp using machine learning',
+        link: 'https://github.com/itsskofficial/Machine-Learning/blob/master/Natural%20Language%20Processing/project.ipynb',
+        github: 'https://github.com/itsskofficial/Machine-Learning/blob/master/Natural%20Language%20Processing/project.ipynb',
+        image: '/assets/images/yelp-review.png'
+    },
+    {
+        name: 'Ethereum Faucet',
+        id: 8,
+        description: 'A faucet to donate and receive virtual Ethereum coins',
+        link: 'https://ethereum-faucet-blush.vercel.app/',
+        github: 'https://github.com/itsskofficial/Web3/tree/master/faucet',
+        image: '/assets/images/eth-faucet.png'
+    },
+    {
+        name: 'Ethereum Course Marketplace',
+        id: 9,
+        description: 'A marketplace to explore and buy courses with Ethereum',
+        link: 'https://ethereum-faucet-blush.vercel.app/',
+        github: 'https://github.com/itsskofficial/Web3/tree/master/course-marketplace',
+        image: '/assets/images/eth-course-marketplace.png'
+    },
+    {
+        name: '911 Calls Analysis',
+        id: 10,
+        description: 'A data science project to analyze 911 calls',
+        link: 'https://github.com/itsskofficial/Data-Science/blob/master/Capstone%20Projects/project1.ipynb',
+        github: 'https://github.com/itsskofficial/Data-Science/blob/master/Capstone%20Projects/project1.ipynb',
+        image: '/assets/images/911-calls-analysis.png'
+    },
+    {
+        name: 'Stock Price Analysis',
+        id: 11,
+        description: 'A data science project to analyze stock prices',
+        link: 'https://github.com/itsskofficial/Data-Science/blob/master/Capstone%20Projects/project2.ipynb',
+        github: 'https://github.com/itsskofficial/Data-Science/blob/master/Capstone%20Projects/project2.ipynb',
+        image: '/assets/images/stock-price-analysis.png'
+    },
+    {
+        name: 'Google Meet Bot',
+        id: 12,
+        description: 'A bot which attends your Google Meets for you',
+        link: 'https://github.com/itsskofficial/Python/edit/master/Google%20Meet%20Bot/',
+        github: 'https://github.com/itsskofficial/Python/edit/master/Google%20Meet%20Bot/',
+        image: '/assets/images/google-meet-bot.png'
+    },
 ]
 
 export default projectsData
