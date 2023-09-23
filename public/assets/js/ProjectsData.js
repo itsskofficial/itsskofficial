b71fa970874cb4b0b35973bc50c897dae25adec5
SK <sarthakkarandikar03@gmail.com>
2023-09-23 08:52:10 +0530

    new projects

--- public/assets/js/ProjectsData.js
+++ public/assets/js/ProjectsData.js
@@ -16,8 +16,24 @@ const projectsData = [
         image: '/assets/images/passion8.png'
     },
     {
-        name: 'Aesthetic Art',
+        name: 'Promptopia',
         id: 3,
+        description: 'A full stack AI prompt sharing application made as a follow along with custom features',
+        link: 'https://prompt-share-itsskofficial.vercel.app/',
+        github: 'https://github.com/itsskofficial/ReactJS/tree/master/prompt-share',
+        image: '/assets/images/promptopia.png'
+    },
+    {
+        name: 'AI Image Generator',
+        id: 4,
+        description: 'A full stack DALL-E kind of clone to generate images with prompts and share it with others',
+        link: 'https://ai-img-gen-iota.vercel.app/',
+        github: 'https://github.com/itsskofficial/ReactJS/tree/master/ai-img-gen',
+        image: '/assets/images/aesthetic-art.png'
+    },
+    {
+        name: 'Aesthetic Art',
+        id: 5,
         description: 'A landing page I made for a customized gifts small business. Made using HTML, CSS & Vanilla JS',
         link: 'https://aesthetic-art.github.io',
         github: 'https://github.com/Aesthetic-Art/Aesthetic-Art.github.io',
@@ -25,7 +41,7 @@ const projectsData = [
     },
     {
         name: 'Campy',
-        id: 4,
+        id: 6,
         description: 'A full stack project which lets you add and rate campgrounds',
         link: 'https://github.com/itsskofficial/Web2/tree/master/campy',
         github: 'https://github.com/itsskofficial/Web2/tree/master/campy',
@@ -33,7 +49,7 @@ const projectsData = [
     },
     {
         name: 'OST Placements',
-        id: 5,
+        id: 7,
         description: 'A landing page I made for a customized gifts small business. Made using HTML, CSS & Vanilla JS',
         link: 'https://blog-landing-lake.vercel.app/',
         github: 'https://github.com/itsskofficial/web2/ost-placements/',
@@ -41,7 +57,7 @@ const projectsData = [
     },
     {
         name: 'Lending Prediction Model',
-        id: 6,
+        id: 8,
         description: 'A model to predict the right loan receivers using machine learning',
         link: 'https://github.com/itsskofficial/Machine-Learning/blob/master/Decision%20Trees%20And%20Random%20Forests/project.ipynb',
         github: 'https://github.com/itsskofficial/Machine-Learning/blob/master/Decision%20Trees%20And%20Random%20Forests/project.ipynb',
@@ -49,7 +65,7 @@ const projectsData = [
     },
     {
         name: 'Yelp Review Model',
-        id: 7,
+        id: 9,
         description: 'A model to predict the rating on Yelp using machine learning',
         link: 'https://github.com/itsskofficial/Machine-Learning/blob/master/Natural%20Language%20Processing/project.ipynb',
         github: 'https://github.com/itsskofficial/Machine-Learning/blob/master/Natural%20Language%20Processing/project.ipynb',
@@ -57,7 +73,7 @@ const projectsData = [
     },
     {
         name: 'Ethereum Faucet',
-        id: 8,
+        id: 10,
         description: 'A faucet to donate and receive virtual Ethereum coins',
         link: 'https://ethereum-faucet-blush.vercel.app/',
         github: 'https://github.com/itsskofficial/Web3/tree/master/faucet',
@@ -65,7 +81,7 @@ const projectsData = [
     },
     {
         name: 'Ethereum Course Marketplace',
-        id: 9,
+        id: 11,
         description: 'A marketplace to explore and buy courses with Ethereum',
         link: 'https://ethereum-course-marketplace-itsskofficial.vercel.app/',
         github: 'https://github.com/itsskofficial/Web3/tree/master/course-marketplace',
@@ -73,7 +89,7 @@ const projectsData = [
     },
     {
         name: 'Food Vision Model',
-        id: 10,
+        id: 12,
         description: 'A deep learning model to predict food item type based on images',
         link: 'https://github.com/itsskofficial/Deep-Learning/blob/master/CNN/Food%20Vision/food_vision.ipynb',
         github: 'https://github.com/itsskofficial/Deep-Learning/blob/master/CNN/Food%20Vision/food_vision.ipynb',
@@ -81,7 +97,7 @@ const projectsData = [
     },
     {
         name: 'Skim Lit',
-        id: 11,
+        id: 13,
         description: 'A deep learning model to skim through medical research papers and classify the contents into categories',
         link: 'https://github.com/itsskofficial/Deep-Learning/blob/master/NLP/Skim%20Lit/skim_lit.ipynb',
         github: 'https://github.com/itsskofficial/Deep-Learning/blob/master/NLP/Skim%20Lit/skim_lit.ipynb',
@@ -89,7 +105,7 @@ const projectsData = [
     },
     {
         name: 'Bit Predict',
-        id: 12,
+        id: 14,
         description: 'A deep learning model to predict Bitcoin future price based on past prices',
         link: 'https://github.com/itsskofficial/Deep-Learning/blob/master/NLP/Bit%20Predict/bit-predict.ipynb',
         github: 'https://github.com/itsskofficial/Deep-Learning/blob/master/CNN/Food%20Vision/bit-predict.ipynb',
@@ -97,7 +113,7 @@ const projectsData = [
     },
     {
         name: '911 Calls Analysis',
-        id: 13,
+        id: 15,
         description: 'A data science project to analyze 911 calls',
         link: 'https://github.com/itsskofficial/Data-Science/blob/master/Capstone%20Projects/project1.ipynb',
         github: 'https://github.com/itsskofficial/Data-Science/blob/master/Capstone%20Projects/project1.ipynb',
@@ -105,7 +121,7 @@ const projectsData = [
     },
     {
         name: 'Stock Price Analysis',
-        id: 14,
+        id: 16,
         description: 'A data science project to analyze stock prices',
         link: 'https://github.com/itsskofficial/Data-Science/blob/master/Capstone%20Projects/project2.ipynb',
         github: 'https://github.com/itsskofficial/Data-Science/blob/master/Capstone%20Projects/project2.ipynb',
@@ -113,7 +129,7 @@ const projectsData = [
     },
     {
         name: 'Google Meet Bot',
-        id: 15,
+        id: 17,
         description: 'A bot which attends your Google Meets for you',
         link: 'https://github.com/itsskofficial/Python/edit/master/Google%20Meet%20Bot/',
         github: 'https://github.com/itsskofficial/Python/edit/master/Google%20Meet%20Bot/',
